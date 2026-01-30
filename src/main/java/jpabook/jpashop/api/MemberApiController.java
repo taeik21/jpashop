e9d5c66a2e95e3b124eb6d08e2a1db40a02eb936
taeik <kti1452@naver.com>
2026-01-30 19:47:16 +0900

    feat: Implement member registration api

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/members")
+    public CreateMemberResponse saveMember(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long memberId = memberService.join(member);
+        return new CreateMemberResponse(memberId);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+
+}
